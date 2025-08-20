d15743e7860e197dfd99afe504fd3c34bc7036c6
Your Name <anurag6009singh@gmail.com>
2025-08-20 13:47:16 +0530

    first comit

---
+++ entities/user.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Vehicle;
+
+struct User
+{
+    string userId;
+    string name;
+    string aadharCard;
+    vector<Vehicle *> vehicles;
+};
